230b6c10e45d8d61feb5d32ab9a541b9b105d6a5
Punk_Off <pankov.kirill1997@yandex.ru>
2022-05-29 13:53:38 +0300

    work with fragments

--- app/src/main/java/com/example/fragment_example_3/ListFragment.kt
+++ app/src/main/java/com/example/fragment_example_3/ListFragment.kt
@@ -1,15 +1,17 @@
 package com.example.fragment_example_3
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 
-class ListFragment: Fragment() {
+class ListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,11 +24,24 @@ class ListFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val imageView = view.findViewById<ImageView>(R.id.imageView1)
+        val imageView1 = view.findViewById<ImageView>(R.id.imageView1)
+        val imageView2 = view.findViewById<ImageView>(R.id.imageView2)
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            imageView1.setOnClickListener {
+                parentFragmentManager.beginTransaction().replace(R.id.mail_container, MailFragment()).commit()
+                Toast.makeText(context, "ImageView1", Toast.LENGTH_SHORT).show()
+            }
 
-        imageView.setOnClickListener {
-            startActivity(Intent(context, MainActivity2::class.java))
-    }
+            imageView2.setOnClickListener {
+                parentFragmentManager.beginTransaction().replace(R.id.mail_container, DescriptionFragment()).commit()
+                Toast.makeText(context, "ImageView2", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            imageView1.setOnClickListener {
+                startActivity(Intent(context, MainActivity2::class.java))
+            }
+        }
 
     }
 }
